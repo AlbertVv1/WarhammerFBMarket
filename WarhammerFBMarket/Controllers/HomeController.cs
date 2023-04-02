7a6dce19188f3e9527108c1e887fba057a3555cc
Albert <mosgfrrfgsom@gmail.com>
2023-04-02 15:52:33 +0300

    Сделана навигация по сайту, можно посмотреть товары.

--- WarhammerFBMarket/Controllers/HomeController.cs
+++ WarhammerFBMarket/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WarhammerFBMarket.Controllers
 			return View();
 		}
 
+		public IActionResult About()
+		{
+			return View();
+		}
+
 		public IActionResult Privacy()
 		{
 			return View();
